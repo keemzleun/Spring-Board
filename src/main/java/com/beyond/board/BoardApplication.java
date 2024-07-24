1d8dc912ea5f9b443434484f16981e22499f5804
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-07-24 20:50:27 +0900

    board

--- src/main/java/com/beyond/board/BoardApplication.java
+++ src/main/java/com/beyond/board/BoardApplication.java
@@ -4,7 +4,9 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession // 세션 스토리지로 레디스를 사용하겠다는 설정
 @EnableBatchProcessing	// Batch 사용시 선언
 @EnableScheduling // 스케줄러에 사용되는 어노테이션
 @SpringBootApplication
